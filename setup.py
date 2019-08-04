36483009f33ab8bedeff1f6e19c77abf34d054eb
Alex <a.serban@cs.ru.nl>
2019-08-04 23:31:57 +0200

    Changed pip name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='py-easticinfra',
+    name='pyeasticinfrastructure',
     version='1.0',
     description='A small utilty to index infrastructure metrics to elasticsearch',
     author='NullConvergence',
